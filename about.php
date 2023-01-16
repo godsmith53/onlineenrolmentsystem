6457297892903a552d84c89394ea69e0c6181e91
godsmith53 <toaditya07@gmail.com>
2023-01-16 15:52:47 +0530

    Initial Commit

---
+++ about.php
@@ -0,0 +1,46 @@
+   <!-- /.row -->
+          
+                     
+            <div class="row">
+                 
+                <div class="col-lg-12">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3><b>  Mission and Vision</b>
+                           </h3>
+                        </div>
+                        <!-- /.panel-heading -->
+                        <div class="panel-body">
+                            <p>
+                               <h1> VISION</h1>
+
+                               For over decade SVCE has provided peerless academic, administrative and pastoral support to students hoping to succeed and beyond. From academic aspirations to the simple day-to-day challenges of living, we understand the importance of every part of the student journey. SVCE is about much more than just teaching we are driven by the desire to help our students find their own path to a lifetime of success. We do this by focusing as much on the personal development of our students as we do on their academic results.
+
+                                     
+
+                                   <h1>  MISSION</h1>
+
+                                    Sri Venkakteshwara College Of Engineering is committed to develop academic and technopreneurial competencies, facilitate learning experiences and assure the highest standards of quality and excellence and institutional integrity.
+
+                                     
+
+                                   <h1> GOALS</h1>
+                                   <ul>
+                                    <li>Greatly recognize the need of achieving orderliness, discipline and respect among its internal and external stakeholders in the attainment of the harmonious interpersonal relationship and spirit of oneness.</li>
+                                    <li>Highly adhere to its existing policies and standards, rules and regulations as to promote a culture of discipline, i.e.., discipline people, discipline thoughts and discipline actions.</li>
+                                    <li>Profoundly advance quality and excellence in its academic and technopreneurial programs through national and international accreditation.</li>
+                                    <li>Eminently pursue local and global networking with institutions of higher, Technical – Vocational and basic education.</li>
+                                    <li>Energetically promote national and international affiliations, partnership and joint ventures with other organizations to enhance institutional capabilities and competencies.</li>
+                                    <li>Apply acknowledge and honor excellence among its internal and external stakeholders, institutional partners country-wide and worldwide.</li>
+                                    <li>Deeply cultivate and develop the essentiality and continuous relevant research and extension activities among its personnel and students to promote innovations, inventions and novelties.</li>
+                                    <li>Collectively strive and sustain the establishment of Sri Venkateshwara College Of Engineering as a world-class institution from good to great and built to last.</li>
+                                   </ul>
+                                </p>
+                        </div>
+                        <!-- /.panel-body -->
+                    </div>
+                    <!-- /.panel -->
+                </div>
+                <!-- /.col-lg-6 -->
+            </div>
+           
---
+++ contact.php
@@ -0,0 +1,39 @@
+ <!-- Map Column -->
+            <div class="col-md-8">
+ College Location
+                <!-- Embedded Google Map -->
+                <!-- <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed/v1/place?q=green%20valley%20%20General%20Paulino%20Santos%20Drive%2C%20Koronadal%20City%2C%209506%20South%20Cotabato%2C%20Philippines"></iframe> -->
+           <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=false"></script>
+           <div class="mapouter"><div class="gmap_canvas"><iframe width="533" height="569" id="gmap_canvas" src="https://maps.google.com/maps?q=SVCE,Bengaluru&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://123movies-to.org"></a><br><style>.mapouter{position:relative;text-align:right;height:569px;width:533px;}</style><a href="https://www.embedgooglemap.net">using google maps on your website</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:569px;width:533px;}</style></div></div>
+            </div>
+            <!-- Contact Details Column -->
+            <div class="col-md-4">
+                <h3>Contact Details</h3>
+                <p>
+                    Sri Venkateshwara College Of Engineering
+                <br>Vidyanagar Cross,Bengaluru Pin-562157<br>
+                </p>
+                <p><i class="fa fa-phone"></i> 
+                    <abbr title="Phone">P</abbr>: +919731226262</p>
+                <p><i class="fa fa-envelope-o"></i> 
+                    <abbr title="Email">Email</abbr>: <a href="mailto:name@example.com">admissions@svcengg.edu.in</a>
+                </p>
+                <p><i class="fa fa-clock-o"></i> 
+                    <abbr title="Hours">H</abbr>: Monday - Friday: 9:00 AM to 5:00 PM</p>
+                <ul class="list-unstyled list-inline list-social-icons">
+                    <li>
+                        <a href="https://www.facebook.com/svcebangalore"><i class="fa fa-facebook-square fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/school/sri-venkateshwara-college-of-engineering-bangalore/about/"><i class="fa fa-linkedin-square fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/SvceBengaluru"><i class="fa fa-twitter-square fa-2x"></i></a>
+                    </li>
+                    <li>
+                        <a href="#"><i class="fa fa-google-plus-square fa-2x"></i></a>
+                    </li>
+                </ul>
+            </div>
+
+ 
